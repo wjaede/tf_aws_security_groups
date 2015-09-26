215391c58b5767839f3cb8305eeecc4aa309f7ea
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-09-26 11:08:15 +0200

    fixed rebase-problem

--- output.tf
+++ output.tf
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-output "sg_ssh_id" {
-  value = "${aws_security_group.ssh.id}"
-}
 
-output "sg_http_id" {
-  value = "${aws_security_group.http.id}"
-}
-
-output "sg_icmp_id" {
-  value = "${aws_security_group.icmp.id}"
-}
-
-output "sg_ovpn_id" {
-  value = "${aws_security_group.ovpn.id}"
-}
-
-output "sg_internal_id" {
-  value = "${aws_security_group.internal.id}"
-=======
 #output "sg_ssh_id" {
 #  value = "${aws_security_group.ssh.id}"
 #}
@@ -44,5 +25,4 @@ output "internal_id" {
 
 output "public_id" {
   value = "${aws_security_group.public.id}"
->>>>>>> namespache changed
 }
