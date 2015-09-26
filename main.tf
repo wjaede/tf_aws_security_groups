1178aa986fc21d9471458c9f167a6ebcbbee7cf1
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-09-26 19:06:23 +0200

    multi regeion update

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+/* Setup our aws provider */
+provider "aws" {
+  access_key  = "${var.access_key}"
+  secret_key  = "${var.secret_key}"
+  region      = "${var.region}"
+}
 #/* Default security group */
 #resource "aws_security_group" "internal" {
 #  name = "security-group-internal"
--- variables.tf
+++ variables.tf
@@ -1,1 +1,5 @@
 variable "vpc_id" {}
+/* provider */
+variable "region"               {}
+variable "access_key"           {}
+variable "secret_key"           {}
