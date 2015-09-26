a957dfd832b9fc9f49133712c97e8d35579d528f
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-09-26 09:54:06 +0200

    namespache changed

--- main.tf
+++ main.tf
@@ -1,3 +1,158 @@
+<<<<<<< HEAD
+=======
+#/* Default security group */
+#resource "aws_security_group" "internal" {
+#  name = "security-group-internal"
+#  description = "Default security group that allows inbound and outbound traffic from all instances in the VPC"
+#  vpc_id = "${var.vpc_id}"
+#
+#  ingress {
+#    from_port   = "0"
+#    to_port     = "0"
+#    protocol    = "-1"
+#    self        = true
+#  }
+#
+#  egress {
+#    from_port   = "0"
+#    to_port     = "0"
+#    protocol    = "-1"
+#    self        = true
+#  }
+#
+#  tags {
+#	Name = "sg-internal"
+#  }
+#}
+#
+#/* Default security group */
+#resource "aws_security_group" "icmp" {
+#  name = "security-group-icmp"
+#  description = "Default security group that allows inbound and outbound traffic from all instances in the VPC"
+#  vpc_id = "${var.vpc_id}"
+#
+#  ingress {
+#    from_port = -1
+#    to_port   = -1
+#    protocol  = "icmp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port = -1
+#    to_port   = -1
+#    protocol  = "icmp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  tags {
+#	Name = "sg-icmp"
+#  }
+#}
+#/* Security group */
+#resource "aws_security_group" "ovpn" {
+#  name = "security-group-ovpn"
+#  description = "Security group for nat instances that allows VPN connections from internet."
+#  vpc_id = "${var.vpc_id}"
+#
+#  ingress {
+#    from_port = 22
+#    to_port   = 22
+#    protocol  = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  ingress {
+#    from_port = -1
+#    to_port   = -1
+#    protocol  = "icmp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port = -1
+#    to_port   = -1
+#    protocol  = "icmp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  ingress {
+#    from_port = 1194
+#    to_port   = 1194
+#    protocol  = "udp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  tags {
+#	Name = "sg-ovpn"
+#  }
+#}
+#
+#/* Security group */
+#resource "aws_security_group" "ssh" {
+#  name = "security-group-ssh"
+#  description = "Security group for nat instances that allows SSH traffic"
+#  vpc_id = "${var.vpc_id}"
+#
+#  ingress {
+#	from_port = 22
+#	to_port   = 22
+#	protocol  = "tcp"
+#	cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  /*
+#  egress {
+#	from_port = 22
+#	to_port   = 22
+#	protocol  = "tcp"
+#	cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  */
+#  tags {
+#	Name = "sg-ssh"
+#  }
+#}
+#
+#/* Security group */
+#resource "aws_security_group" "http" {
+#  name = "security-group-http"
+#  description = "Security group for nat instances that allows HTTP(s) traffic"
+#  vpc_id = "${var.vpc_id}"
+#
+#  ingress {
+#	from_port = 80
+#	to_port   = 80
+#	protocol  = "tcp"
+#	cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#	from_port = 80
+#	to_port   = 80
+#	protocol  = "tcp"
+#	cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  ingress {
+#	from_port = 443
+#	to_port   = 443
+#	protocol  = "tcp"
+#	cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#	from_port = 443
+#	to_port   = 443
+#	protocol  = "tcp"
+#	cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  tags {
+#	Name = "sg-http"
+#  }
+#}
+
+>>>>>>> namespache changed
 /* Default security group */
 resource "aws_security_group" "internal" {
   name = "security-group-internal"
@@ -5,6 +160,7 @@ resource "aws_security_group" "internal" {
   vpc_id = "${var.vpc_id}"
 
   ingress {
+<<<<<<< HEAD
 	from_port   = "0"
 	to_port     = "0"
 	protocol    = "-1"
@@ -127,3 +283,75 @@ resource "aws_security_group" "http" {
   }
 }
 
+=======
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    self        = true
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    self        = true
+  }
+
+  tags {
+    Name = "sg-internal"
+  }
+}
+
+/* Security group for the nat server */
+resource "aws_security_group" "public" {
+  name = "security-group-public"
+  description = "Security group for nat instances that allows SSH and VPN traffic from internet. Also allows outbound HTTP[S]"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port   = -1
+    protocol  = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = -1
+    to_port   = -1
+    protocol  = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 1194
+    to_port   = 1194
+    protocol  = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "sg-public"
+  }
+}
+>>>>>>> namespache changed
--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 output "sg_ssh_id" {
   value = "${aws_security_group.ssh.id}"
 }
@@ -16,4 +17,32 @@ output "sg_ovpn_id" {
 
 output "sg_internal_id" {
   value = "${aws_security_group.internal.id}"
+=======
+#output "sg_ssh_id" {
+#  value = "${aws_security_group.ssh.id}"
+#}
+#
+#output "sg_http_id" {
+#  value = "${aws_security_group.http.id}"
+#}
+#
+#output "sg_icmp_id" {
+#  value = "${aws_security_group.icmp.id}"
+#}
+#
+#output "sg_ovpn_id" {
+#  value = "${aws_security_group.ovpn.id}"
+#}
+#
+#output "sg_internal_id" {
+#  value = "${aws_security_group.internal.id}"
+#}
+
+output "internal_id" {
+  value = "${aws_security_group.internal.id}"
+}
+
+output "public_id" {
+  value = "${aws_security_group.public.id}"
+>>>>>>> namespache changed
 }
